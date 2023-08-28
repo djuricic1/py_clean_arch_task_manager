78a77c46ed2c8beb8c29b026f75de2cec5e24a31
Dejan Duricic <dejan@helu.io>
2023-08-28 19:05:22 +0200

    Add payload for create task endpoint

--- task_manager/domain/entities/task.py
+++ task_manager/domain/entities/task.py
@@ -1,6 +1,6 @@
 import datetime
-from dataclasses import dataclass, asdict
-from uuid import UUID
+from dataclasses import dataclass, asdict, field
+from uuid import UUID, uuid4
 
 from task_manager.domain.enums import Priority
 from task_manager.domain.enums import Status
@@ -8,13 +8,13 @@ from task_manager.domain.enums import Status
 
 @dataclass
 class Task:
-    id: UUID
     title: str
     description: str
     due_date: datetime.date
     priority: Priority
     status: Status
     assignee_id: UUID
+    id: UUID = field(default_factory=uuid4)
 
     @classmethod
     def create_from_dict(cls, data: dict) -> "Task":
--- task_manager/web/app.py
+++ task_manager/web/app.py
@@ -4,6 +4,7 @@ from typing import Annotated, Type
 from uuid import UUID
 
 from fastapi import FastAPI, Depends
+from pydantic import BaseModel
 
 from task_manager.domain.entities import Task, User
 from task_manager.domain.enums.task_priority import Priority
@@ -20,23 +21,25 @@ def get_repository_factory() -> IRepositoryFactory:
     return MemRepoFactory([])
 
 
+class TaskCreatePayload(BaseModel):
+    title: str
+    description: str
+    due_date: date
+    priority: Priority
+    status: Status
+    assignee_id: UUID
+
+
 @rest_app.post("/task/", response_model=None)
 def create_task(
     repository_factory: Annotated[
         IRepositoryFactory, Depends(get_repository_factory)
     ],
+    task: TaskCreatePayload,
 ) -> Task:
     in_memory_task_repo = repository_factory.create_repository(Type[Task])
 
-    random_task = Task(
-        id=UUID("b4c57439-bde0-461e-b811-c37f7abe21a0"),
-        title="Complete Project Report",
-        description="Write a detailed project report for the quarterly review.",
-        due_date=date(2023, 8, 25),
-        priority=Priority.HIGH,
-        status=Status.IN_PROGRESS,
-        assignee_id=UUID("b4c57439-bde0-461e-b811-c37f7abe21a0"),
-    )
+    random_task = Task.create_from_dict(task.model_dump())
 
     result = create_task_use_case(repo=in_memory_task_repo, task=random_task)
 
