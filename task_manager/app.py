0a02a655474aef35b5a26a70b389586d7a22e9e2
Dejan Duricic <dejan@helu.io>
2023-08-28 12:38:51 +0200

    Add update and delete routes

--- task_manager/app.py
+++ task_manager/app.py
@@ -7,4 +7,4 @@ app: FastAPI = FastAPI()
 app.mount("", rest_app)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run("app:app", host="0.0.0.0", port=80, reload=True)
--- task_manager/web/app.py
+++ task_manager/web/app.py
@@ -23,7 +23,7 @@ def create_task(
     repository_factory: Annotated[
         IRepositoryFactory, Depends(get_repository_factory)
     ],
-):
+) -> Task:
     in_memory_task_repo = repository_factory.create_repository(Type[Task])
 
     random_task = Task(
@@ -46,6 +46,49 @@ def get_tasks(
     repository_factory: Annotated[
         IRepositoryFactory, Depends(get_repository_factory)
     ],
-):
+) -> list[Task]:
     in_memory_task_repo = repository_factory.create_repository(Type[Task])
     return in_memory_task_repo.get_all()
+
+
+@rest_app.get("/task/{task_id}")
+def get_task_by_id(
+    repository_factory: Annotated[
+        IRepositoryFactory, Depends(get_repository_factory)
+    ],
+    task_id: UUID,
+):
+    in_memory_task_repo = repository_factory.create_repository(Type[Task])
+    return in_memory_task_repo.get_by_id(task_id)
+
+
+@rest_app.post("/task/{task_id}")
+def update_task_by_id(
+    repository_factory: Annotated[
+        IRepositoryFactory, Depends(get_repository_factory)
+    ],
+    task_id: UUID,
+):
+    random_task = Task(
+        id=UUID("b4c57439-bde0-461e-b811-c37f7abe21a0"),
+        title="Complete Project Report",
+        description="Write a detailed project report for the quarterly review.",
+        due_date=date(2023, 8, 25),
+        priority=Priority.HIGH,
+        status=Status.COMPLETED,
+        assignee_id=UUID("b4c57439-bde0-461e-b811-c37f7abe21a0"),
+    )
+
+    in_memory_task_repo = repository_factory.create_repository(Type[Task])
+    return in_memory_task_repo.update(random_task)
+
+
+@rest_app.delete("/task/{task_id}")
+def delete_task_by_id(
+    repository_factory: Annotated[
+        IRepositoryFactory, Depends(get_repository_factory)
+    ],
+    task_id: UUID,
+):
+    in_memory_task_repo = repository_factory.create_repository(Type[Task])
+    return in_memory_task_repo.delete(task_id)
