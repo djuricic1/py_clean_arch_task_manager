ce92c75e348ba19373fc3e1a5a8bce1ac7e9f4eb
Dejan Duricic <dejan@helu.io>
2023-09-08 21:01:51 +0200

    Add scripts.py for application running

--- task_manager/app.py
+++ task_manager/app.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI
 
 from task_manager.web.routers import task_router, user_router
@@ -6,6 +5,3 @@ from task_manager.web.routers import task_router, user_router
 app: FastAPI = FastAPI()
 app.include_router(task_router)
 app.include_router(user_router)
-
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=80, reload=True)
